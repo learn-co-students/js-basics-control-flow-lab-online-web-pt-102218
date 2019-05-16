00012bc31facded7b9598eb288ccefb9bb39ab5b
Paul Ashour <paul.ashour@gmail.com>
2019-05-16 09:32:36 -0600

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(_feet){
   // Write your code here!
+  if (_feet <= 400){
+    return 'This one is on me!';
+  } else if (_feet <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  }
 }
 
 function ternaryCheckCity(){
