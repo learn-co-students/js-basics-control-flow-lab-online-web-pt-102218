a82ed38b90618cd4da26ff83d0ef193e63fe5f01
alexandercleasby <cleasby.alex@gmail.com>
2019-05-20 19:11:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if (feet > 2500) { return 'No can do.'}
+  return feet <= 400 ? 'This one is on me!' : 'I will gladly take your thirty bucks.'
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip){
+    case 'generous':return 'Thank you so much.'
+    case 'not as generous':return 'Thank you.'
+  }
+  return 'Bye.'
 }
