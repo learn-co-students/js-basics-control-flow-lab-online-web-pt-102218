bfcb98ed841ba2e25d61b073886a3b645a1a1bdb
beaulieu527 <beaulieudev@gmail.com>
2019-05-31 20:17:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  let greeting = ""
+
+  if (feet <= 400){
+    greeting = "This one is on me!"
+  } 
+  else if(feet > 2500){
+    greeting = "No can do."
+  }
+  else if(feet > 2000){
+    greeting = "I will gladly take your thirty bucks."
+  } 
+  return greeting
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+
+ return city === "NYC" ? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip){
+    case 'generous':
+      return "Thank you so much."
+    case 'not as generous':
+      return "Thank you."
+    default:
+      return "Bye."
+  }
 }
